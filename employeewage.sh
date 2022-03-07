798d6207a4ba4ec94fc68f9e6287dcd73df47743
P DILLI RANI <pasupulatidillirani@gmail.com>
2022-03-07 16:45:37 +0530

    employeewage:

--- employeewage.sh
+++ employeewage.sh
@@ -224,29 +224,4 @@ static void Main(string[] args)
 
 
 
-^G Help          ^O Write Out     ^W Where Is      ^K Cut           ^T Execute       ^C Location      M-U Undo         M-A Set Mark     M-] To Bracket   M-Q Previous     ^B Back
-^X Exit          ^R Read File     ^\ Replace       ^U Paste         ^J Justify       ^/ Go To Line    M-E Redo         M-6 Copy         ^Q Where Was     M-W Next         ^F Forwa
 
-\\int Present = 1;
-int fulltime = 1;
-int Emp_per_rate = 20;
-int emphr = 0;
-int empwage = 0;
-
-Random random = new Random();
-Random random1 = new Random();
-int EmployeeStatus = random.Next(0, 2);
-int EmployeeTime = random1.Next(0, 2);
-if (EmployeeStatus == Present)
-{
-    if (EmployeeTime == fulltime)
-    {
-        Console.WriteLine("Employee is present");
-        emphr = 8;
-    }
-    else
-    {
-        Console.WriteLine("Employee  is present");
-        emphr = 4;
-    }
-}
